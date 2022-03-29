599ec161688146f4d64e31727d4525219ac724eb
cerwantes <max1162@web.de>
2022-03-29 13:55:50 +0200

    changed delimiter

--- src/Generator/GoogleShopping.php
+++ src/Generator/GoogleShopping.php
@@ -364,27 +364,16 @@ class GoogleShopping extends CSVPluginGenerator
         $pattern = $variationAttributes[self::CHARACTER_TYPE_PATTERN];
 
         $additionalData = "";
-        if ((!empty($color)) or (!empty($size)) or (!empty($pattern))) {
-            $additionalData = " | ";
+        $delimiter = "|";
 
-            if (!empty($color)) {
-                $additionalData .= $color;
-                if (!empty($size) and !empty($pattern)) {
-                    $additionalData .= ", " . $size . ", " . $pattern;
-                } elseif (!empty($size)) {
-                    $additionalData .= ", " . $size;
-                } elseif (!empty($pattern)) {
-                    $additionalData .= ", " . $pattern;
-                }
-
-            } elseif (!empty($size)) {
-                $additionalData .= $size;
-                if (!empty($pattern)) {
-                    $additionalData .= ", " . $pattern;
-                }
-            } elseif (!empty($pattern)) {
-                $additionalData .= $pattern;
-            }
+        if(!empty($color)){
+            $additionalData .= $delimiter . " " . $color;
+        }
+        if(!empty($size)){
+            $additionalData .= $delimiter . " " . $size;
+        }
+        if(!empty($pattern)){
+            $additionalData .= $delimiter . " " . $pattern;
         }
 
         if (empty($size)) {
@@ -397,7 +386,7 @@ class GoogleShopping extends CSVPluginGenerator
 
         $data = [
             'id' => $this->elasticExportHelper->generateSku($variation['id'], self::GOOGLE_SHOPPING, 0, $variation['data']['skus']['sku']),
-            'title' => $this->elasticExportHelper->getMutatedName($variation, $settings, 256) . $additionalData,
+            'title' => $this->elasticExportHelper->getMutatedName($variation, $settings, 256) . " " . $additionalData,
             'description' => $this->getDescription($variation, $settings),
             'google_product_category' => $this->elasticExportHelper->getCategoryMarketplace((int)$variation['data']['defaultCategories'][0]['id'], (int)$settings->get('plentyId'), 129),
             'product_type' => $this->elasticExportHelper->getCategory((int)$variation['data']['defaultCategories'][0]['id'], (string)$settings->get('lang'), (int)$settings->get('plentyId')),
